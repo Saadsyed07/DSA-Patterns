1577bf232a4bf90d757635029ec98d1bf97bec74
Saadsyed07 <saadsyed33922@gmail.com>
2025-09-12 22:11:44 +0530

    updated

--- 6 Stack/07 Asteroid Collision.cpp
+++ 6 Stack/07 Asteroid Collision.cpp
@@ -1,44 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution{
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int>s;
+    vector<int> asteroidCollision(vector<int> &asteroids){
+        stack<int> s;
 
-      for(int asteroid : asteroids){
-        if(asteroid > 0){
-            s.push(asteroid);
-        }else{
-            while(!s.empty() && s.top() > 0 && s.top()< -asteroid){
-                s.pop();
-            }
-            if(s.empty() || s.top() < 0){
+        for (int asteroid : asteroids){
+            if (asteroid > 0){
                 s.push(asteroid);
             }
-            if(!s.empty() && s.top() == -asteroid){
-                s.pop();
+            else{
+                while (!s.empty() && s.top() > 0 && s.top() < -asteroid){
+                    s.pop();
+                }
+                if (s.empty() || s.top() < 0){
+                    s.push(asteroid);
+                }
+                if (!s.empty() && s.top() == -asteroid){
+                    s.pop();
+                }
             }
         }
-      }
 
-      vector<int> ans (s.size());
-      int i = s.size() -1;
-      while(!s.empty()){
-        ans[i--] = s.top();
-        s.pop();
-      }
+        vector<int> ans(s.size());
+        int i = s.size() - 1;
+        while (!s.empty()){
+            ans[i--] = s.top();
+            s.pop();
+        }
 
-      return ans;
+        return ans;
     }
 };
 int main(){
-Solution s;
-vector<int> asteroids = {5,10,-5};
-vector<int> ans = s.asteroidCollision(asteroids);
-for(int num : ans){
-    cout<<num<<" ";
-}
+    Solution s;
+    vector<int> asteroids = {5, 10, -5};
+    vector<int> ans = s.asteroidCollision(asteroids);
+    for (int num : ans){
+        cout << num << " ";
+    }
 
-return 0;
+    return 0;
 }
